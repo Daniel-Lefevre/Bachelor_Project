baa307da22e6796ad18f5b0d65f124f2c2ed83c8
WilliamNB2003 <williamnobo2003@gmail.com>
2026-02-17 22:04:52 +0100

    FirstPartOfAnimationAdded

--- src/BusinessLayer/system.py
+++ src/BusinessLayer/system.py
@@ -5,18 +5,18 @@ import keyboard
 
 class System:
     def __init__(self, ips, positions):
-        self.RobotArms = []
+        # self.RobotArms = []
         self.running = True
         self.storageObjects = configuration["StorageObjects"]
         self.DT = DTRunner()
         self.lock = threading.Lock()
 
         # Add all the robot arms
-        for i in range(len(ips)):
-            ID = i
-            IP_address = ips[i]
-            poses = positions[i]
-            self.RobotArms.append(RobotArm(IP_address, poses, ID))
+        # for i in range(len(ips)):
+        #     ID = i
+        #     IP_address = ips[i]
+        #     poses = positions[i]
+        #     self.RobotArms.append(RobotArm(IP_address, poses, ID))
 
     def stopSystem(self):
         print("STOP")
@@ -29,48 +29,48 @@ class System:
 
         self.threads = []
 
-        t_ir = threading.Thread(target=self.IR_Listener)
-        self.threads.append(t_ir)
-        t_ir.start()
+        # t_ir = threading.Thread(target=self.IR_Listener)
+        # self.threads.append(t_ir)
+        # t_ir.start()
 
 
-        for arm in self.RobotArms:
-            t = threading.Thread(target=self.robot_worker, args=(arm,))
-            self.threads.append(t)
-            t.start()
+        # for arm in self.RobotArms:
+        #     t = threading.Thread(target=self.robot_worker, args=(arm,))
+        #     self.threads.append(t)
+        #     t.start()
 
-        t_anomaly = threading.Thread(target=self.anomalyListener)
-        self.threads.append(t_anomaly)
-        t_anomaly.start()
+        # t_anomaly = threading.Thread(target=self.anomalyListener)
+        # self.threads.append(t_anomaly)
+        # t_anomaly.start()
 
-        self.startupRobotsLoop()
+        # self.startupRobotsLoop()
 
-    def robot_worker(self, arm):
-        # Setup Phase
-        print(f"Robot {arm.ID} is initializing")
-        arm.setUp()
-        print(f"Robot {arm.ID} setup finished")
+    # def robot_worker(self, arm):
+    #     # Setup Phase
+    #     print(f"Robot {arm.ID} is initializing")
+    #     arm.setUp()
+    #     print(f"Robot {arm.ID} setup finished")
 
-        # 2. Monitoring Phase
-        while self.running:
-            arm.Loop()
-            time.sleep(0.05)
+    #     # 2. Monitoring Phase
+    #     while self.running:
+    #         arm.Loop()
+    #         time.sleep(0.05)
 
-        # Disconnect when not running
-        arm.disconnect()
-        print("Everything has been shut down")
+    #     # Disconnect when not running
+    #     arm.disconnect()
+    #     print("Everything has been shut down")
 
     def startupRobotsLoop(self):
         while self.running:
             # Robot 0 take image
-            if keyboard.is_pressed('9'):
-                self.RobotArms[0].takeImage()
-                time.sleep(0.5)
+            # if keyboard.is_pressed('9'):
+            #     self.RobotArms[0].takeImage()
+            #     time.sleep(0.5)
 
-            # Robot 1 take image
-            elif keyboard.is_pressed('2'):
-                self.RobotArms[1].takeImage()
-                time.sleep(0.5)
+            # # Robot 1 take image
+            # elif keyboard.is_pressed('2'):
+            #     self.RobotArms[1].takeImage()
+            #     time.sleep(0.5)
 
             time.sleep(0.05)
 
@@ -87,9 +87,9 @@ class System:
 
     def getObjects(self):
         # Retrieve updates from the robot arms
-        for arm in self.RobotArms:
-            for update in arm.getObjectUpdates():
-                self.updateObject(*update)
+        # for arm in self.RobotArms:
+        #     for update in arm.getObjectUpdates():
+        #         self.updateObject(*update)
 
         return self.storageObjects
 
@@ -105,12 +105,12 @@ class System:
             self.DT.createEvent(("Pick Up", object))
 
             # Tell to pick up from storage
-            self.RobotArms[int(object.position[-1])].addToQueue(configuration["PickFromStoragePriority"], "Storage", object.shape, object.color)
+            # self.RobotArms[int(object.position[-1])].addToQueue(configuration["PickFromStoragePriority"], "Storage", object.shape, object.color)
 
             rules = self.makeRuleFromEvent(object, destination)
 
-            for i in range(len(self.RobotArms)):
-                self.RobotArms[i].setRules(rules[i])
+            # for i in range(len(self.RobotArms)):
+            #     self.RobotArms[i].setRules(rules[i])
 
             self.DT.setRules(rules)
 
@@ -129,61 +129,61 @@ class System:
 
         return rules
 
-    def anomalyListener(self):
-        while (self.running):
-            for robotArm in self.RobotArms:
-                messages = robotArm.getAnomalyUpdates()
-                for message in messages:
-                    if (message[0] == "Stop System"):
-                        self.stopSystem()
-                        print("Human Intervention Required")
-                    elif (message[0] == "Anomaly 5"):
-                        id, shape, color = message[1]
-                        self.anomaly5Mitigation(id, shape, color)
+    # def anomalyListener(self):
+    #     while (self.running):
+    #         for robotArm in self.RobotArms:
+    #             messages = robotArm.getAnomalyUpdates()
+    #             for message in messages:
+    #                 if (message[0] == "Stop System"):
+    #                     self.stopSystem()
+    #                     print("Human Intervention Required")
+    #                 elif (message[0] == "Anomaly 5"):
+    #                     id, shape, color = message[1]
+    #                     self.anomaly5Mitigation(id, shape, color)
 
-            time.sleep(0.1)
+    #         time.sleep(0.1)
 
-    def anomaly5Mitigation(self, robotIDArrival, shape, color):
-        print(f"ID: {robotIDArrival}")
-        goalStorageID = None
-        for storageObject in self.storageObjects:
-            if (storageObject.shape == shape and storageObject.color == color):
-                if storageObject.position == "In_Transit":
-                    for robotId in range(len(self.RobotArms)):
-                        if (self.RobotArms[robotId].getRules().get((shape, color)) == "Storage"):
-                            goalStorageID = robotId
-                else:
-                    print("happend")
-                    goalStorageID = int(storageObject.position[-1])
-                    print(goalStorageID)
-                break
-        if (goalStorageID == robotIDArrival):
-            print("TRUE")
-            self.RobotArms[robotIDArrival].setRules({(shape, color): "Storage"})
-            self.RobotArms[robotIDArrival].addToQueue(configuration["PickFromIRSensorPriority"], "Conveyor", shape, color)
-            self.RobotArms[robotIDArrival].setMitigationMode(False)
-        else:
-            print("False")
-            self.RobotArms[robotIDArrival].setRules({(shape, color): "Conveyor"})
-            self.RobotArms[int(not robotIDArrival)].setRules({(shape, color): "Storage"})
-            self.RobotArms[robotIDArrival].addToQueue(configuration["PickFromIRSensorPriority"], "Conveyor", shape, color)
-            self.RobotArms[robotIDArrival].setMitigationMode(False)
+    # def anomaly5Mitigation(self, robotIDArrival, shape, color):
+    #     print(f"ID: {robotIDArrival}")
+    #     goalStorageID = None
+    #     for storageObject in self.storageObjects:
+    #         if (storageObject.shape == shape and storageObject.color == color):
+    #             if storageObject.position == "In_Transit":
+    #                 for robotId in range(len(self.RobotArms)):
+    #                     if (self.RobotArms[robotId].getRules().get((shape, color)) == "Storage"):
+    #                         goalStorageID = robotId
+    #             else:
+    #                 print("happend")
+    #                 goalStorageID = int(storageObject.position[-1])
+    #                 print(goalStorageID)
+    #             break
+    #     if (goalStorageID == robotIDArrival):
+    #         print("TRUE")
+    #         self.RobotArms[robotIDArrival].setRules({(shape, color): "Storage"})
+    #         self.RobotArms[robotIDArrival].addToQueue(configuration["PickFromIRSensorPriority"], "Conveyor", shape, color)
+    #         self.RobotArms[robotIDArrival].setMitigationMode(False)
+    #     else:
+    #         print("False")
+    #         self.RobotArms[robotIDArrival].setRules({(shape, color): "Conveyor"})
+    #         self.RobotArms[int(not robotIDArrival)].setRules({(shape, color): "Storage"})
+    #         self.RobotArms[robotIDArrival].addToQueue(configuration["PickFromIRSensorPriority"], "Conveyor", shape, color)
+    #         self.RobotArms[robotIDArrival].setMitigationMode(False)
 
 
-    def IR_Listener(self):
-        has_received_false = [False, False]
-        while(self.running):
-            for ID in range(len(self.RobotArms)):
-                robotArm = self.RobotArms[ID]
-                if (robotArm.getIR()):
-                    if (has_received_false[ID]):
-                        with self.lock:
-                            self.DT.createEvent(("IR", ID))
-                        has_received_false[ID] = False
-                else:
-                    has_received_false[ID] = True
+    # def IR_Listener(self):
+    #     has_received_false = [False, False]
+    #     while(self.running):
+    #         for ID in range(len(self.RobotArms)):
+    #             robotArm = self.RobotArms[ID]
+    #             if (robotArm.getIR()):
+    #                 if (has_received_false[ID]):
+    #                     with self.lock:
+    #                         self.DT.createEvent(("IR", ID))
+    #                     has_received_false[ID] = False
+    #             else:
+    #                 has_received_false[ID] = True
 
-            time.sleep(0.1)
+    #         time.sleep(0.1)
 
 
 
--- src/PresentationLayer/GUI.py
+++ src/PresentationLayer/GUI.py
@@ -1,10 +1,68 @@
-import customtkinter as ctk
 from PIL import Image, ImageOps
+from src.PresentationLayer.Animation import *
+import customtkinter as ctk
+
 class GUI:
     def __init__(self, system):
         self.system = system
         self.storageObjects = system.getObjects()
+        
+        self.loadImages()
 
+
+        # Color palette
+        self.bg_color = "#495057"      # Stacks (Medium Grey)
+        self.box_color = "#6C757D"     # Item Rows (Light Grey)
+        self.title_color = "#F8F9FA"   # Headers (Bright White)
+        self.text_color = "#E9ECEF"    # Item Text (Soft White)
+        self.border_color = "#ADB5BD"  # Borders (Brighter highlight)
+
+        # Create window
+        self.root = ctk.CTk(fg_color=self.bg_color)
+        self.root.title("Storage Overview")
+        w = self.root.winfo_screenwidth()
+        h = self.root.winfo_screenheight()
+        self.root.geometry(f"{w}x{h}")
+
+        # Labels for stacks
+        self.labels = ["Storage_0", "In_Transit", "Storage_1"]
+
+        # Keep references to stack frames so we can update them later
+        self.frames = {}
+        self.last_state = {}
+
+        for col, label in enumerate(self.labels):
+            stack = self.create_stack(self.root, label, self.box_color)
+            if col == 0:
+                stack.grid(row=0, column=col, padx=15, pady=15, sticky="nsew")
+            elif col == 1:
+                stack.grid(row=0, column=col, columnspan=2, padx=15, pady=15, sticky="nsew")
+            elif col == 2:
+                stack.grid(row=0, column=col+1, padx=15, pady=15, sticky="nsew")
+            self.frames[label] = stack
+        
+        self.Animation = Animation(self.root, self.storageObjects)
+
+
+        # Make the grid cells expand
+        gridConfigs = [2,1,1,2]
+        for i in range(4):
+            self.root.grid_columnconfigure(i, weight=gridConfigs[i])
+
+        for i in range(2):
+            self.root.grid_rowconfigure(i, weight=1)
+
+
+        self.createAnomalyLog()
+        self.createButtons()
+
+        # Start the periodic update loop
+        self.updateStorageObjects()
+
+        # Start the main GUI loop
+        self.root.mainloop()
+
+    def loadImages(self):
         # Load the images
         diff_img = Image.open("resources/Images/different_storage.png").convert("RGBA")
         same_img = Image.open("resources/Images/same_storage.png").convert("RGBA")
@@ -25,49 +83,94 @@ class GUI:
             size=(20, 20)
         )
 
-        # Color palette
-        self.bg_color = "#495057"      # Stacks (Medium Grey)
-        self.box_color = "#6C757D"     # Item Rows (Light Grey)
-        self.title_color = "#F8F9FA"   # Headers (Bright White)
-        self.text_color = "#E9ECEF"    # Item Text (Soft White)
-        self.border_color = "#ADB5BD"  # Borders (Brighter highlight)
+    def createButtons(self):
+        DT_frame = ctk.CTkFrame(self.root, fg_color=self.box_color, corner_radius=10, width=100, height=100)
+        DT_frame.pack_propagate(False)
 
-        # Create window
-        self.root = ctk.CTk(fg_color=self.bg_color)
-        self.root.title("Storage Overview")
-        self.root.geometry("900x500")
+        ctk.CTkLabel(
+            DT_frame,
+            text="Logs",
+            font=("Arial", 16, "bold"),
+            fg_color="transparent",
+            text_color=self.title_color
+        ).pack(pady=(10, 0))
 
-        # Labels for stacks
-        self.labels = ["Storage_0", "In_Transit", "Storage_1"]
-
-        # Keep references to stack frames so we can update them later
-        self.frames = {}
-        self.last_state = {}
-
-        for col, label in enumerate(self.labels):
-            stack = self.create_stack(self.root, label, self.box_color)
-            stack.grid(row=0, column=col, padx=15, pady=15, sticky="nsew")
-            self.frames[label] = stack
-
-        # Make the grid cells expand
-        for i in range(3):
-            self.root.grid_rowconfigure(i, weight=1)
-            self.root.grid_columnconfigure(i, weight=1)
-
-        exit_btn = ctk.CTkButton(
-            self.root,
-            fg_color="red",
+        timeBasedDTButton = ctk.CTkButton(
+            DT_frame,
+            fg_color=self.bg_color,
             hover_color=self.border_color,
-            text="Shut down Robots",
-            command=lambda : self.system.stopSystem()
+            text="Time based DT",
+            command=lambda : self.system.stopSystem(),
+            width=120,
+            height=35
         )
-        exit_btn.grid(row=1, column=2, padx=15, pady=(0, 15), sticky="se")
+        timeBasedDTButton.pack(pady=(10, 10))
 
-        # Start the periodic update loop
-        self.update_loop()
+        visionBasedDTButton = ctk.CTkButton(
+            DT_frame,
+            fg_color=self.bg_color,
+            hover_color=self.border_color,
+            text="Vision based DT",
+            command=lambda : self.system.stopSystem(),
+            width=120,
+            height=35
+        )
+        visionBasedDTButton.pack(pady=(10, 10))
 
-        # Start the main GUI loop
-        self.root.mainloop()
+        noDTButton = ctk.CTkButton(
+            DT_frame,
+            fg_color=self.bg_color,
+            hover_color=self.border_color,
+            text="No DT",
+            command=lambda : self.system.stopSystem(),
+            width=120,
+            height=35
+        )
+        noDTButton.pack(pady=(10, 10))
+
+        ctk.CTkLabel(
+            DT_frame,
+            text="Turn on/off system",
+            font=("Arial", 16, "bold"),
+            fg_color="transparent",
+            text_color=self.title_color
+        ).pack()
+
+        startButton = ctk.CTkButton(
+            DT_frame,
+            fg_color="green",
+            hover_color=self.border_color,
+            text="Vision based DT",
+            command=lambda : self.system.stopSystem(),
+            width=120,
+            height=35
+        )
+        startButton.pack(pady=(10, 10))
+
+        exitButton = ctk.CTkButton(
+            DT_frame,
+            fg_color="#880015",
+            hover_color=self.border_color,
+            text="Shut down system",
+            command=lambda : self.system.stopSystem(),
+            width=120,
+            height=35
+        )
+        exitButton.pack(pady=(10, 10))
+        DT_frame.grid(row=1, column=2, sticky="news", padx=15, pady=15)
+
+    def createAnomalyLog(self):
+        log_frame = ctk.CTkFrame(self.root, fg_color=self.box_color, corner_radius=10)
+        # log_frame.pack_propagate(False)
+
+        ctk.CTkLabel(
+            log_frame,
+            text="Logs",
+            font=("Arial", 16, "bold"),
+            fg_color="transparent",
+            text_color=self.title_color
+        ).pack(pady=(10, 10))
+        log_frame.grid(row=1, column=3, sticky="news", padx=15, pady=15)
 
     # Function to "color" a black icon to white
     def make_white(self, img):
@@ -76,7 +179,7 @@ class GUI:
         return Image.merge("RGBA", (ImageOps.invert(r), ImageOps.invert(g), ImageOps.invert(b), a))
 
     def create_stack(self, parent, title, color):
-        stack_frame = ctk.CTkFrame(parent, fg_color=color, corner_radius=10, height=400)
+        stack_frame = ctk.CTkFrame(parent, fg_color=color, corner_radius=10)
         stack_frame.pack_propagate(False)
 
         newTitle = ""
@@ -131,7 +234,7 @@ class GUI:
                 ).grid(row=0, column=0, pady=6)
 
                 for j in range(1, 4, 2):
-                    separator = ctk.CTkFrame(row_frame, fg_color=self.border_color, height=45, width=2)
+                    separator = ctk.CTkFrame(row_frame, fg_color=self.border_color, height=35, width=2)
                     separator.grid(row=0, column=j, pady=5)
 
                 for j in range(2, 5, 2):
@@ -150,7 +253,7 @@ class GUI:
                     )
                     btn.grid(row=0, column=j)
 
-    def update_loop(self):
+    def updateStorageObjects(self):
         # Refresh object list
         self.storageObjects = self.system.getObjects()
 
@@ -171,8 +274,9 @@ class GUI:
                 self.last_state[label] = current_signature
 
         # Call again after 500ms
-        self.root.after(500, self.update_loop)
+        self.root.after(500, self.updateStorageObjects)
 
     def clicked(self, params):
-        self.system.moveObject(*params)
+        # self.system.moveObject(*params)
+        pass
 
